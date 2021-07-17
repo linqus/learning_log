8148349a22c00d3243cde6b4400fb9a0a124f6fa
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-17 22:29:23 +0200

    lista wpisów na stronie tematu

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,6 @@ app_name = 'learning_logs_app'
 urlpatterns = [
 	path('', views.index, name='index' ),
 	path('topics/', views.topics, name='tematy'),
-
+	path('topics/<int:id_tematu>/',views.topic, name='temat'),
+	
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -7,8 +7,13 @@ def index(request):
 	return render(request,'learning_logs/index.html')
 
 def topics(request):
-
 	topics = Topic.objects.order_by('date_added')
 	context = {'topics':topics}
-
 	return render(request,'learning_logs/topics.html',context)
+
+
+def topic(request,**kwargs):
+	topic = Topic.objects.get(id=kwargs['id_tematu'])
+	entries = topic.entry_set.order_by('-date_added')
+	context ={'temat': topic, 'wpisy':entries} 
+	return render(request,'learning_logs/topic.html',context)
