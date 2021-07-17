e16ece05a383a28cbc23798eb52032b8d573beed
Karol Linkiewicz <karol@linkiewicz.pl>
2021-07-17 16:03:10 +0200

    lista tematów (topics)

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'learning_logs'
+app_name = 'learning_logs_app'
 urlpatterns = [
 	path('', views.index, name='index' ),
-	
+	path('topics/', views.topics, name='tematy'),
+
 ]
