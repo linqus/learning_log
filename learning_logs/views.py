d4aff4062877150fc7f4c27fd12c5ae2786e3c2e
holograph@data.pl <Gitpapa45>
2021-07-19 16:51:48 +0200

    tematy widoczne tylko dla właścicieli

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm,EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
@@ -10,13 +11,15 @@ def index(request):
 
 @login_required
 def topics(request):
-	topics = Topic.objects.order_by('date_added')
+	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics':topics}
 	return render(request,'learning_logs/topics.html',context)
 
 @login_required
 def topic(request,**kwargs):
 	topic = Topic.objects.get(id=kwargs['id_tematu'])
+	if topic.owner != request.user:
+		raise Http404
 	entries = topic.entry_set.order_by('-date_added')
 	context ={'temat': topic, 'wpisy':entries} 
 	return render(request,'learning_logs/topic.html',context)
@@ -28,7 +31,9 @@ def new_topic(request):
 	else:
 		form = TopicForm(data=request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner=request.user
+			new_topic.save()
 			return redirect('learning_logs_app:tematy')
 
 	context = {'form':form}
@@ -54,6 +59,8 @@ def new_entry(request,id_tematu):
 def edit_entry(request,id_wpisu):
 	entry = Entry.objects.get(id=id_wpisu)
 	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
 	if request.method != 'POST':
 		form = EntryForm(instance=entry)
 	else:
