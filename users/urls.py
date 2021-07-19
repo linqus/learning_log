cce8d541f6dcc36568bb8948a8f5fff4001d958e
holograph@data.pl <Gitpapa45>
2021-07-19 16:06:14 +0200

    funkcjonalność rejestrowania użytkownika: widok, url, linki + literówki

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-
+from . import views 
 
 app_name = 'użytkownik'
 urlpatterns = [
 	path('', include('django.contrib.auth.urls')),
+	path('register/',views.register,name='rejestracja')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+def register(request):
+	
+	if request.method != 'POST':
+		formularz = UserCreationForm()
+	else:
+		formularz = UserCreationForm(data=request.POST)
+		if formularz.is_valid():
+			new_user = formularz.save()
+			login(request,new_user)
+			return redirect('learning_logs_app:index')
+		
+	context = {'form':formularz}
+	return render(request,'registration/register.html',context)	
